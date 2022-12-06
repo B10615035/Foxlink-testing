152786f71dcc43e5d92f68c41650357a398069bd
rorubyy <roruby118@gmail.com>
2022-12-06 19:49:35 +0800

    foox bugs

--- scripts/testings/envs.sh
+++ scripts/testings/envs.sh
@@ -3,7 +3,4 @@ set -a
 IS_APP=1
 SCENARIO="Reject"
 SCENARIO_DB_TAG="basic"
-set +aOnce a user is diagnosed by a diagnostician, the user has
-options to exchange its existing pseudonyms with the current
-handler by giving the patient’s consent to this very specific
-diagnosticianc
+set +a
